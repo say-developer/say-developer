b3e8ff6af9428ac3094a459d85051622beed943f
Benjamin Rae <benjaminrae93@gmail.com>
2023-10-04 17:12:34 +0200

    ci(workflows): create github workflows
    Create Build, Commitlint, Lint and Test workflows

--- packages/backend/src/main.ts
+++ packages/backend/src/main.ts
@@ -1,12 +1,19 @@
 import { NestFactory } from '@nestjs/core';
 import { SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
-import { swaggerConfig, swaggerDocumentOptions } from './configs/docs/swagger.config';
+import {
+  swaggerConfig,
+  swaggerDocumentOptions,
+} from './configs/docs/swagger.config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  const documents = SwaggerModule.createDocument(app, swaggerConfig, swaggerDocumentOptions);
+  const documents = SwaggerModule.createDocument(
+    app,
+    swaggerConfig,
+    swaggerDocumentOptions,
+  );
   SwaggerModule.setup('docs', app, documents);
 
   await app.listen(3000);
